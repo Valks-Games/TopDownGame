6387794ffc9e77a9baa5c57446ca1651f638e12f
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-06-23 14:31:45 -0500

    Cleanup chunk.cs / no logic changes

--- Scripts/RTS/Chunk.cs
+++ Scripts/RTS/Chunk.cs
@@ -1,51 +1,20 @@
-﻿using static System.Net.Mime.MediaTypeNames;
-
-namespace RTS;
+﻿namespace RTS;
 
 public class Chunk
 {
+    int chunkX, chunkY;
+
     public Chunk(Node parent, int chunkX, int chunkY)
     {
-        GenerateMesh(parent, new Vector2(chunkX, chunkY), World.ChunkSize);
-        //this.GenerateChunk(chunkX, chunkY);
+        this.chunkX = chunkX;
+        this.chunkY = chunkY;
+
+        GenerateMesh(parent);
     }
 
-    void SetUVs(ref Vector2[] uvs, Vector2 texSize)
+    void GenerateMesh(Node parent)
     {
-        // The texture has 8 x 15 tiles
-        // Each tile is 16 pixels in size
-        var texTiles = new Vector2(8, 15);
-
-        var tileX = 0;
-        var tileY = 0;
-
-        var tileWidth = World.TileSize / texSize.X;
-        var tileHeight = World.TileSize / texSize.Y;
-
-        for (int i = 0; i < uvs.Length; i += 4)
-        {
-            //GD.Print($"({tileX}, {tileY})");
-
-            var x = (World.TileSize * tileX) / texSize.X;
-            var y = (World.TileSize * tileY) / texSize.Y;
-
-            uvs[i] = new Vector2(x, y);
-            uvs[i + 1] = new Vector2(x, y + tileHeight);
-            uvs[i + 2] = new Vector2(x + tileWidth, y + tileHeight);
-            uvs[i + 3] = new Vector2(x + tileWidth, y);
-
-            tileX += 1;
-
-            if (tileX % 8 == 0)
-            {
-                tileX -= 8;
-                tileY += 1;
-            } 
-        }
-    }
-
-    void GenerateMesh(Node parent, Vector2 chunkCoords, int size)
-    {
+        var size = World.ChunkSize;
         var vertices = new Vector3[4 * size * size];
         //var normals  = new Vector3[4 * size * size];
         var uvs      = new Vector2[4 * size * size];
@@ -53,23 +22,18 @@ public class Chunk
         var indices  = new     int[6 * size * size];
 
         for (int n = 0; n < colors.Length; n++)
-        {
             colors[n] = Colors.White;
-        }
 
         for (int m = 0; m < uvs.Length; m++)
-        {
             uvs[m] = new Vector2(0, 0);
-        }
 
         var tex = GD.Load<Texture2D>("res://Sprites/basictiles.png");
-
-        SetUVs(ref uvs, tex.GetSize());
 
         var s = World.TileSize / 2; // hard coded size
         var w = s * 2; // width
 
         var chunkSize = w * size;
+        var chunkCoords = new Vector2(chunkX, chunkY);
         var tileChunkPos = chunkCoords * chunkSize; // (6400, 6400)
         var chunkPos = chunkCoords * size; // (100, 100)
 
@@ -81,24 +45,22 @@ public class Chunk
                 y: s + (-chunkSize / 2) + tileChunkPos.Y, 
                 z: 0);
 
+        SetUVs(ref uvs, tex.GetSize());
+
         var i = 0;
         var v = 0;
+
+        GD.Print(uvs.Length);  // 400
+        GD.Print(size * size); // 100
 
         for (int z = 0; z < size; z++)
         {
             for (int x = 0; x < size; x++)
             {
                 vertices[v] = new Vector3(-s, -s, 0) + posVec3;
-                //normals     [v] = new Vector3( 0, 0,  s);
-
                 vertices[v + 1] = new Vector3(-s, s, 0) + posVec3;
-                //normals [v + 1] = new Vector3( 0, 0, s );
-
                 vertices[v + 2] = new Vector3(s, s, 0) + posVec3;
-                //normals [v + 2] = new Vector3( 0, 0, s );
-
                 vertices[v + 3] = new Vector3(s, -s, 0) + posVec3;
-                //normals [v + 3] = new Vector3( 0, 0, s );
 
                 indices[i] = v;
                 indices[i + 1] = v + 1;
@@ -107,6 +69,11 @@ public class Chunk
                 indices[i + 3] = v + 2;
                 indices[i + 4] = v + 3;
                 indices[i + 5] = v + 0;
+
+                //normals     [v] = new Vector3( 0, 0,  s);
+                //normals [v + 1] = new Vector3( 0, 0, s );
+                //normals [v + 2] = new Vector3( 0, 0, s );
+                //normals [v + 3] = new Vector3( 0, 0, s );
 
                 v += 4;
                 i += 6;
@@ -140,42 +107,37 @@ public class Chunk
         parent.AddChild(meshInstance);
     }
 
-    void GenerateChunk(int chunkX, int chunkY)
+    void SetUVs(ref Vector2[] uvs, Vector2 texSize)
     {
-        foreach (var atlas in World.Atlases)
-            for (int x = 0; x < World.ChunkSize; x++)
-                for (int y = 0; y < World.ChunkSize; y++)
-                    GenerateTile(chunkX, chunkY, x, y, atlas);
-    }
+        // The texture has 8 x 15 tiles
+        // Each tile is 16 pixels in size
+        var texTiles = new Vector2(8, 15);
 
-    void GenerateTile(int chunkX, int chunkY, int x, int y, Atlas atlas)
-    {
-        var globalX = (chunkX * World.ChunkSize) + x;
-        var globalY = (chunkY * World.ChunkSize) + y;
+        var tileX = 0;
+        var tileY = 0;
 
-        string type = "";
-        var currentNoise = atlas.FNL.GetNoise2D(globalX, globalY);
+        var tileWidth = World.TileSize / texSize.X;
+        var tileHeight = World.TileSize / texSize.Y;
 
-        foreach (var atlasValue in atlas.TileData)
+        for (int i = 0; i < uvs.Length; i += 4)
         {
-            if (currentNoise < atlasValue.Value.Weight)
+            //GD.Print($"({tileX}, {tileY})");
+
+            var x = (World.TileSize * tileX) / texSize.X;
+            var y = (World.TileSize * tileY) / texSize.Y;
+
+            uvs[i] = new Vector2(x, y);
+            uvs[i + 1] = new Vector2(x, y + tileHeight);
+            uvs[i + 2] = new Vector2(x + tileWidth, y + tileHeight);
+            uvs[i + 3] = new Vector2(x + tileWidth, y);
+
+            tileX += 1;
+
+            if (tileX % 8 == 0)
             {
-                type = atlasValue.Key;
-                break;
+                tileX -= 8;
+                tileY += 1;
             }
         }
-
-        SetCell(
-            atlas.TileMap, 
-            type,
-            globalX, 
-            globalY, 
-            atlas.TileData[type].TilePosition);
-    }
-
-    void SetCell(TileMap tileMap, string typeName, int x, int y, Vector2I atlasPos)
-    {
-        if (typeName != "empty")
-            tileMap.SetCell(0, new Vector2I(x, y), 0, atlasPos);
     }
 }
