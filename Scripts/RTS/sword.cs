12355338896a8cc373f0d151de071d8ea181b4a9
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-06-27 12:53:57 -0500

    Remove pre-generated comments from Sword.cs

--- Scripts/RTS/sword.cs
+++ Scripts/RTS/sword.cs
@@ -7,7 +7,6 @@ public partial class Sword : Sprite2D
     private int dir;
     private GTween tween;
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         center = GetParent<Node2D>();
@@ -16,7 +15,6 @@ public partial class Sword : Sprite2D
         tween.Pause();
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _PhysicsProcess(double delta)
     {
         //center.LookAt(GetGlobalMousePosition());
